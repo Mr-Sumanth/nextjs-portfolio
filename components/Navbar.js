3a1cea58979c0d9204b8e78b8979ae211132dcf8
Mr-Sumanth <csumanth81@gmail.com>
2025-05-24 11:30:53 +0530

    bugs are fixed

--- components/Navbar.js
+++ components/Navbar.js
@@ -14,8 +14,8 @@ const Navbar = () => {
     
        <ul className=' flex gap-1  rounded-full items-center justify-center relative bottom-3 '>
         <li><Link href="/"><button type="button" class="text-white  bg-gradient-to-br rounded-full from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium text-[8px] sm:text-[12px] xl:text-sm px-2.5 sm:px-5 py-2.5 text-center me-2 mb-2 ">Home</button> </Link>  </li>
-        <li><Link href="/Skills-Projects"><button type="button" class="text-white bg-gradient-to-br rounded-full from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium text-[7px] sm:text-[12px] xl:text-sm px-2.5 sm:px-5 py-2.5 text-center me-2 mb-2"><li className='flex flex-col'>Skills & Projects</li></button></Link></li>
-        <li><Link href="/Resume"><button type="button" class="text-white bg-gradient-to-br rounded-full from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium text-[8px] sm:text-[12px] xl:text-sm px-2.5 sm:px-5 py-2.5 text-center me-2 mb-2">Resume</button></Link></li>
+        <li><Link href="/skillsprojects"><button type="button" class="text-white bg-gradient-to-br rounded-full from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium text-[7px] sm:text-[12px] xl:text-sm px-2.5 sm:px-5 py-2.5 text-center me-2 mb-2"><li className='flex flex-col'>Skills & Projects</li></button></Link></li>
+        <li><Link href="/resume"><button type="button" class="text-white bg-gradient-to-br rounded-full from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium text-[8px] sm:text-[12px] xl:text-sm px-2.5 sm:px-5 py-2.5 text-center me-2 mb-2">Resume</button></Link></li>
         
       
        </ul>
